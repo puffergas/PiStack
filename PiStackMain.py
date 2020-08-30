728115d69961add4935748f5a46b6310d8caa5f9
Jeff <jeff@puffergas.com>
2020-08-29 23:18:45 -0400

    Switch button now switches both freq.

--- PiStackMain.py
+++ PiStackMain.py
@@ -4,9 +4,10 @@ import PySimpleGUI as sg
 sg.theme('DarkBlack1') 
    
 layout = [
-    [sg.Text('COM1              NAV1')], 
-    [sg.Text(size=(7,1), key='-USE-', text_color='#6D9F85', background_color='white'), sg.Button('Switch'),
-        sg.Input(size=(7, 1), key='-STBYcomm1-')], 
+    [sg.Text('COM1')], 
+    [sg.Input(size=(7,1), key='-USE-', readonly=True, border_width=(4), text_color='red',
+        background_color='white'), sg.Button('Switch'), sg.Input(size=(7, 1),
+        text_color='red', border_width=(4), key='-STBYcomm1-')], 
     [sg.Button('Exit')]
 ] 
   
@@ -22,6 +23,7 @@ while True:
     if event == 'Switch': 
         # Update the "output" text element 
         # to be the value of "input" element 
-        window['-USE-'].update(values['-STBYcomm1-']) 
+        window['-USE-'].update(values['-STBYcomm1-']),
+        window['-STBYcomm1-'].update(values['-USE-'])
   
 window.close() 
