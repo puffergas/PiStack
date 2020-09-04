46072ddff9bf723fc64986cefe07969d0f450fe7
Jeff <jeff@puffergas.com>
2020-09-03 22:44:57 -0400

    adding nav1

--- PiStackMain.py
+++ PiStackMain.py
@@ -5,9 +5,12 @@ sg.theme('DarkBlack1')
    
 layout = [
     [sg.Text('COM1')], 
-    [sg.Input(size=(7,1), key='-USE-', readonly=True, border_width=(4), text_color='red',
+    [sg.Input(size=(7,1), key='-USEcom1-', readonly=True, border_width=(4), text_color='red',
         background_color='white'), sg.Button('Switch'), sg.Input(size=(7, 1),
-        text_color='red', border_width=(4), key='-STBYcomm1-')], 
+        text_color='red', border_width=(4), key='-STBYcom1-'),
+            sg.Input(size=(7,1), key='-USEnav1-', readonly=True, border_width=(4), text_color='red',
+            background_color='white'), sg.Button('Switch'), sg.Input(size=(7, 1),
+            text_color='red', border_width=(4), key='-STBYnav1-')], 
     [sg.Button('Exit')]
 ] 
   
@@ -23,7 +26,7 @@ while True:
     if event == 'Switch': 
         # Update the "output" text element 
         # to be the value of "input" element 
-        window['-USE-'].update(values['-STBYcomm1-']),
-        window['-STBYcomm1-'].update(values['-USE-'])
+        window['-USEcom1-'].update(values['-STBYcom1-']),
+        window['-STBYcom1-'].update(values['-USEcom1-'])
   
 window.close() 
