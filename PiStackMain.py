2361bafa0b1e644c724ec957dedee83f87135a01
Jeff <jeff@puffergas.com>
2020-09-06 21:19:47 -0400

    Added COM2 and NAV2

--- PiStackMain.py
+++ PiStackMain.py
@@ -4,13 +4,20 @@ import PySimpleGUI as sg
 sg.theme('DarkBlack1') 
    
 layout = [
-    [sg.Text('COM1')], 
-    [sg.Input(size=(7,1), key='-USEcom1-', readonly=True, border_width=(4), text_color='red',
-        background_color='white'), sg.Button('Switch'), sg.Input(size=(7, 1),
-        text_color='red', border_width=(4), key='-STBYcom1-'),
-            sg.Input(size=(7,1), key='-USEnav1-', readonly=True, border_width=(4), text_color='red',
-            background_color='white'), sg.Button('Switch'), sg.Input(size=(7, 1),
-            text_color='red', border_width=(4), key='-STBYnav1-')], 
+    [sg.Text('                     COM1                                            NAV1')], 
+    [sg.Input(size=(7,1), key='-USE_COM1-', readonly=True, border_width=(4), text_color='red',
+        background_color='white'), sg.Button('Switch', key='-SWITCH_COM1-'), sg.Input(size=(7, 1),
+        text_color='red', border_width=(4), key='-STBY_COM1-'),
+            sg.Input(size=(7,1), key='-USE_NAV1-', readonly=True, border_width=(4), text_color='red',
+            background_color='white'), sg.Button('Switch', key='-SWITCH_NAV1-'), sg.Input(size=(7, 1),
+            text_color='red', border_width=(4), key='-STBY_NAV1-')],
+    [sg.Text('                     COM2                                            NAV2')], 
+    [sg.Input(size=(7,1), key='-USE_COM2-', readonly=True, border_width=(4), text_color='red',
+        background_color='white'), sg.Button('Switch', key='-SWITCH_COM2-'), sg.Input(size=(7, 1),
+        text_color='red', border_width=(4), key='-STBY_COM2-'),
+            sg.Input(size=(7,1), key='-USE_NAV2-', readonly=True, border_width=(4), text_color='red',
+            background_color='white'), sg.Button('Switch', key='-SWITCH_NAV2-'), sg.Input(size=(7, 1),
+            text_color='red', border_width=(4), key='-STBY_NAV2-')],
     [sg.Button('Exit')]
 ] 
   
@@ -23,10 +30,28 @@ while True:
     if event in  (None, 'Exit'): 
         break
       
-    if event == 'Switch': 
+    if event == '-SWITCH_COM1-': 
         # Update the "output" text element 
         # to be the value of "input" element 
-        window['-USEcom1-'].update(values['-STBYcom1-']),
-        window['-STBYcom1-'].update(values['-USEcom1-'])
+        window['-USE_COM1-'].update(values['-STBY_COM1-']),
+        window['-STBY_COM1-'].update(values['-USE_COM1-'])
+
+    if event == '-SWITCH_NAV1-': 
+        # Update the "output" text element 
+        # to be the value of "input" element 
+        window['-USE_NAV1-'].update(values['-STBY_NAV1-']),
+        window['-STBY_NAV1-'].update(values['-USE_NAV1-'])
+
+    if event == '-SWITCH_COM2-': 
+        # Update the "output" text element 
+        # to be the value of "input" element 
+        window['-USE_COM2-'].update(values['-STBY_COM2-']),
+        window['-STBY_COM2-'].update(values['-USE_COM2-'])
+
+    if event == '-SWITCH_NAV2-': 
+        # Update the "output" text element 
+        # to be the value of "input" element 
+        window['-USE_NAV2-'].update(values['-STBY_NAV2-']),
+        window['-STBY_NAV2-'].update(values['-USE_NAV2-'])
   
 window.close() 
