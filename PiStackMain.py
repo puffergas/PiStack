a40cc3eb849a564cba7edb9549aebe8f925155db
Jeff <jeff@puffergas.com>
2020-09-26 08:35:53 -0400

    Added nav1 and nav2 telnet read

--- PiStackMain.py
+++ PiStackMain.py
@@ -1,8 +1,6 @@
 import PySimpleGUI as sg
 from telnet import FlightGear
  
-boot = True
-
 # fg = FlightGear('myhost', 5501)
 # parking brake on
 # fg['/controls/gear/brake-parking'] = 1
@@ -13,29 +11,43 @@ boot = True
 # selected-mhz = '118.3' (double)
 # selected-mhz-fmt = '118.30' (string)
 # standby-mhz = '119.3' (double)
-# standby-mhz = '119.30' (string)
+# standby-mhz-fmt = '119.30' (string)
 
-fg_com1 = 125.25
-fg_com2 = 130.55
-fg_nav1 = 110.30
-fg_nav2 = 100.85
+fg = FlightGear('localhost', 5401)
+
+fg_com1 = fg['/instrumentation/comm/frequencies/selected-mhz']
+fg_stby_com1 = fg['/instrumentation/comm/frequencies/standby-mhz']
+
+fg_com2 = fg['/instrumentation/comm[1]/frequencies/selected-mhz']
+fg_stby_com2 = fg['/instrumentation/comm[1]/frequencies/standby-mhz']
+
+fg_nav1 = fg['/instrumentation/nav/frequencies/selected-mhz']
+fg_stby_nav1 = fg['/instrumentation/nav/frequencies/standby-mhz']
+
+fg_nav2 = fg['/instrumentation/nav[1]/frequencies/selected-mhz']
+fg_stby_nav2 = fg['/instrumentation/nav[1]/frequencies/standby-mhz']
+
+# fg_com1 = 125.25
+# fg_com2 = 130.55
+# fg_nav1 = 110.30
+# fg_nav2 = 100.85
 
 sg.theme('DarkBlack1')
 
 layout = [
     [sg.Text('                    COM1                                           NAV1')], 
     [sg.Input((fg_com1), size=(7,1), key='-use_com1-', readonly=True, border_width=(4), text_color='red',
-        background_color='white'), sg.Button('<==>', key='-SWITCH_COM1-'), sg.Input(size=(7, 1),
+        background_color='white'), sg.Button('<==>', key='-SWITCH_COM1-'), sg.Input((fg_stby_com1), size=(7, 1),
         text_color='red', border_width=(4), key='-stby_com1-'),
             sg.Input((fg_nav1), size=(7,1), key='-use_nav1-', readonly=True, border_width=(4), text_color='red',
-            background_color='white'), sg.Button('<==>', key='-SWITCH_NAV1-'), sg.Input(size=(7, 1),
+            background_color='white'), sg.Button('<==>', key='-SWITCH_NAV1-'), sg.Input((fg_stby_nav1), size=(7, 1),
             text_color='red', border_width=(4), key='-stby_nav1-')],
     [sg.Text('                    COM2                                           NAV2')], 
     [sg.Input((fg_com2), size=(7,1), key='-use_com2-', readonly=True, border_width=(4), text_color='red',
-        background_color='white'), sg.Button('<==>', key='-SWITCH_COM2-'), sg.Input(size=(7, 1),
+        background_color='white'), sg.Button('<==>', key='-SWITCH_COM2-'), sg.Input((fg_stby_com2), size=(7, 1),
         text_color='red', border_width=(4), key='-stby_com2-'),
-            sg.Input((fg_nav1), size=(7,1), key='-use_nav2-', readonly=True, border_width=(4), text_color='red',
-            background_color='white'), sg.Button('<==>', key='-SWITCH_NAV2-'), sg.Input(size=(7, 1),
+            sg.Input((fg_nav2), size=(7,1), key='-use_nav2-', readonly=True, border_width=(4), text_color='red',
+            background_color='white'), sg.Button('<==>', key='-SWITCH_NAV2-'), sg.Input((fg_stby_nav2), size=(7, 1),
             text_color='red', border_width=(4), key='-stby_nav2-')],
     [sg.Button('Exit')]
 ]
@@ -47,11 +59,7 @@ while True:
     # Un-comment the below to print values to the Python Terminal
     # print(event, values)
     
-    if boot == True:
-        window['-use_com1-'].update(value=555)
-        boot = False,
-        print(boot)
-      
+
     if event in  (None, 'Exit'): 
         break
       
@@ -59,6 +67,8 @@ while True:
         # Swap COM1 frequency
         window['-use_com1-'].update(values['-stby_com1-']),
         window['-stby_com1-'].update(values['-use_com1-'])
+        fg['/instrumentation/comm/frequencies/selected-mhz'] = values['-stby_com1-']
+        fg['/instrumentation/comm/frequencies/standby-mhz'] = values['-use_com1-']
 
     if event == '-SWITCH_NAV1-': 
         # Swap NAV1 frequncy
