12ef15793fa2da6a3f19f0d835b043943fd4abfc
Jeff <jeff@puffergas.com>
2020-10-17 22:24:11 -0400

    Add volume to both COM

--- pistack.py
+++ pistack.py
@@ -29,7 +29,7 @@ except ConnectionRefusedError:
     ' or did not configure FlightGear - PiStack will exit.', font=(any, 14))
     sys.exit()
 
-# Value
+# Value, (double)
 fg_com1 = fg['/instrumentation/comm/frequencies/selected-mhz']
 fg_stby_com1 = fg['/instrumentation/comm/frequencies/standby-mhz']
 fg_com2 = fg['/instrumentation/comm[1]/frequencies/selected-mhz']
@@ -38,6 +38,8 @@ fg_nav1 = fg['/instrumentation/nav/frequencies/selected-mhz']
 fg_stby_nav1 = fg['/instrumentation/nav/frequencies/standby-mhz']
 fg_nav2 = fg['/instrumentation/nav[1]/frequencies/selected-mhz']
 fg_stby_nav2 = fg['/instrumentation/nav[1]/frequencies/standby-mhz']
+fg_vol_com1 = fg['/instrumentation/comm/volume']
+fg_vol_com2 = fg['/instrumentation/comm[1]/volume']
 
 # Boolean 0 or 1
 fg_com1_service = fg['/instrumentation/comm/serviceable']
@@ -53,70 +55,79 @@ frame_layout1 = [
     [sg.Input((fg_com1), size=(7,1), key='-use_com1-', readonly=True, border_width=(4),
     font=('any', 16, 'bold'), text_color='red' if bool(fg_com1_service) else 'Gray',
     background_color='white'),
-
     # Exchange frequncies, button
     sg.Button('<==>', key='-SWITCH_COM1-', font=('any', 14)),
-
-    # Standy by frequency
+    # Standy frequency
     sg.Input((fg_stby_com1), size=(7, 1), font=('any', 16, 'bold'), text_color='red' if bool(fg_com1_service) else 'Gray',
     border_width=(4), key='-stby_com1-'),
     ],
 
-    # On Off, button
+    # On Off, button 
     [sg.Button('On' if bool(fg_com1_service) else 'Off',
     button_color='white on green' if bool(fg_com1_service) else 'white on red',
-    size=(3, 1), key='-COM1_ON_OFF-', font='Any 14'),
+    size=(3, 1), key='-COM1_ON_OFF-', font='Any 14',pad=((6,0),(20,4))),
+    # Volume
+    sg.Slider(range=(0.0, 1.0), resolution=0.1, key='-VOL_COM1-',  default_value=fg_vol_com1, pad=((50,10),(20,4)),
+    size=(10, 34), orientation='horizontal', disable_number_display=True, enable_events=True, font=('any', 12)),
+    sg.Text('Volume', font=('any', 14), pad=((4,4),(20,4))),
     ],
 ]
 
 frame_layout2 = [
-    # NAV1 
+    # -----     NAV1 -----
+    # Active frequency
     [sg.Input((fg_nav1), size=(7,1), key='-use_nav1-', readonly=True, border_width=(4), font=('any', 16, 'bold'),
     text_color='red' if bool(fg_nav1_service) else 'Gray', background_color='white'),
-
+    # Exchange frequncy, button
     sg.Button('<==>', key='-SWITCH_NAV1-', font='Any 14'),
-
+    # Standby frequency
     sg.Input((fg_stby_nav1), size=(7, 1),
     font=('any', 16, 'bold'), text_color='red' if bool(fg_nav1_service) else 'Gray', border_width=(4), key='-stby_nav1-'),
     ],
-
+    # On Off, button
     [sg.Button('On' if bool(fg_nav1_service) else 'Off',
     button_color='white on green' if bool(fg_nav1_service) else 'white on red',
-    size=(3, 1), key='-NAV1_ON_OFF-', font='Any 14'),
+    size=(3, 1), key='-NAV1_ON_OFF-', font='Any 14', pad=((6,0),(20,4))),
     ],
 ]
 
 frame_layout3 = [
-    # COM1
+    # ----- COM2 -----
+    # Active frequncy
     [sg.Input((fg_com2), size=(7,1), key='-use_com2-', readonly=True, border_width=(4), font=('any', 16, 'bold'),
     text_color='red' if bool(fg_com2_service) else 'Gray', background_color='white'),
-
+    # Exchange frequency, button
     sg.Button('<==>', key='-SWITCH_COM2-', font='Any 14'),
-
+    # Standby frequency
     sg.Input((fg_stby_com2), size=(7, 1),
     font=('any', 16, 'bold'), text_color='red' if bool(fg_com2_service) else 'Gray', border_width=(4), key='-stby_com2-'),
     ],
-
+    # On Off, button
     [sg.Button('On' if bool(fg_com2_service) else 'Off',
     button_color='white on green' if bool(fg_com2_service) else 'white on red',
-    size=(3, 1), key='-COM2_ON_OFF-', font='Any 14'),
+    size=(3, 1), key='-COM2_ON_OFF-', font='Any 14', pad=((6,0),(20,4))),
+    # Volume
+    sg.Slider(range=(0.0, 1.0), resolution=0.1, key='-VOL_COM2-',  default_value=fg_vol_com1, pad=((50,10),(20,4)),
+    size=(10, 34), orientation='horizontal', disable_number_display=True, enable_events=True, font=('any', 12)),
+    sg.Text('Volume', font=('any', 14), pad=((4,4),(20,4))),
     ],
 ]
 
 frame_layout4 = [
-    # NAV1 
+    # ----- NAV2 -----
+    # Active frequency
     [sg.Input((fg_nav2), size=(7,1), key='-use_nav2-', readonly=True, border_width=(4), font=('any', 16, 'bold'),
     text_color='red' if bool(fg_nav2_service) else 'Gray', background_color='white'),
-
+    # Exchange frequency, button
     sg.Button('<==>', key='-SWITCH_NAV2-', font='Any 14'),
-
+    # Standby frequency
     sg.Input((fg_stby_nav2), size=(7, 1),
     font=('any', 16, 'bold'), text_color='red' if bool(fg_nav2_service) else 'Gray', border_width=(4), key='-stby_nav2-'),
     ],
-
+    # On Off, button
     [sg.Button('On' if bool(fg_nav2_service) else 'Off',
     button_color='white on green' if bool(fg_nav2_service) else 'white on red',
-    size=(3, 1), key='-NAV2_ON_OFF-', font='Any 14'),
+    size=(3, 1), key='-NAV2_ON_OFF-', font='Any 14', pad=((6,0),(20,4))),
     ],
 ]
 
@@ -203,6 +214,14 @@ while True:
         fg['/instrumentation/nav[1]/frequencies/selected-mhz'] = values1['-stby_nav2-']
         fg['/instrumentation/nav[1]/frequencies/standby-mhz'] = values1['-use_nav2-']
 
+    if event1 == '-VOL_COM1-':
+        # Adjust volume of COM1, slider
+        fg['/instrumentation/comm/volume'] = values1['-VOL_COM1-']
+
+    if event1 == '-VOL_COM2-':
+        # Adjust volume of COM1, slider
+        fg['/instrumentation/comm[1]/volume'] = values1['-VOL_COM2-']
+
     if event1 == '-KEYPAD-' and not window2_active:
         window2_active = True
         window1.Hide()
@@ -235,7 +254,7 @@ while True:
             [sg.Button('Enter'), sg.Button('0'), sg.Button('.'), sg.Cancel()],
             ]
 
-        # Make window visiable
+        # Make Keypad window visiable
         window2 = sg.Window('Keypad', layout2, default_button_element_size=(5,2), auto_size_buttons=False)
 
         keys_entered = ''
